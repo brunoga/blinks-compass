fe5d93c5a5245f3dc344fff2d877fa3d2ff75305
Bruno Albuquerque <bga@gmail.com>
2020-11-23 07:27:00 -0800

    Storage space improvements.
    - Force some commonly used functions to be no-inline.
    - Helps with an upcoming version of Hexxagon.

--- orientation.cpp
+++ orientation.cpp
@@ -8,7 +8,8 @@ static byte face_offset_;
 
 // The face2 parameter is an int8_t because we need to pass a "negative face"
 // when computing an absolute local face.
-static byte positive_face_distance(byte face1, int8_t face2) {
+static byte __attribute__((noinline))
+positive_face_distance(byte face1, int8_t face2) {
   return (face1 - face2 + FACE_COUNT) % FACE_COUNT;
 }
 
@@ -21,7 +22,7 @@ void Setup(byte relative_remote_face, byte absolute_local_face) {
 
 void Reset() { face_offset_ = 0; }
 
-byte RelativeLocalFace(byte absolute_local_face) {
+byte __attribute__((noinline)) RelativeLocalFace(byte absolute_local_face) {
   return positive_face_distance(absolute_local_face, face_offset_);
 }
 
